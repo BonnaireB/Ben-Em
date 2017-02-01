772ed60904707300b7499f32cda259c5eae68573
BonnaireB <bonnaire.benjamin@gmail.com>
2017-02-01 12:11:41 -0500

    ajout de L'énoncé

---
+++ BONB03049706_TP1/BONB03049706_TP1/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  BONB03049706_TP1
+//
+//  Created by Bonnaire Benjamin on 17-01-30.
+//  Copyright © 2017 Bonnaire Benjamin. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    if (argc < 14) {
+        printf("Erreur, le nombre d'entrées est invalide !");
+        return 1;
+    }
+    printf("Hello, World!\n");
+    return 0;
+}
