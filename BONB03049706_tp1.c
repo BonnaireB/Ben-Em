03967fb1f57814749e52278f50263a1c6898b5ff
BonnaireB <bonnaire.benjamin@gmail.com>
2017-02-01 12:44:21 -0500

    Ajout du fichier source du Tp

---
+++ BONB03049706_tp1.c
@@ -0,0 +1,20 @@
+//
+//  BONB03049706_tp1..c
+//  Tp1 INF3135
+//
+//  Created by Bonnaire Benjamin on 17-01-30.
+//  Copyright © 2017 Bonnaire Benjamin. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    if (argc < 14) {
+            printf("Erreur, le nombre d'entrées est invalide !");
+            return 1;
+        }
+    printf("Hello, World!\n");
+    return 0;
+}
