a805a62deab2d9b5265f9cc651eb7aa90fe80be5
BonnaireB <bonnaire.benjamin@gmail.com>
2017-02-05 13:59:10 -0500

    quelques validations

--- validations_diverses.c
+++ validations_diverses.c
@@ -1,115 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-//prend le nombre de notes donnees en entree 
+//prend le nombre de notes donnees en entree
 #define TAILLE argc-1
 #define NOM argv[0]
 
 //valider ce quentre le user est bien des donnees numeros
 void validerNotesFournies(const char *notes);
 //valider que le code permanent fourni respecte le bon format
-void validerCodePerm(char *codePermanent);
+void validerCodePerm(const char* codePerm);
 //valider que notre array de notes recu est dans les limites
 void validerValeursPermises(char *notesPermises, const char *notes);
-
+//remplissage des tableaux des pondeérations et des notes max
+void remplissage ( double tableaux[],int taille, FILE *fichier);
 
 
 
 
 int main(int argc, char *argv[]){
-
+    
     int nombreEval ;
-    char seuils[11];
+    int seuils[11];
+    char charPourInt;
     int iterateur;
     
-
-
-	if (argc != 12) {
-		fprintf(stderr, "Le nombre d'arguments n'est pas valide.\n");
-		exit(EXIT_FAILURE);	
-	}
+    if (argc != 12) {
+        fprintf(stderr, "Le nombre d'arguments n'est pas valide.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    FILE *fichierEntree = stdin;
     
     for (iterateur=1 ; iterateur< argc ;  ++iterateur) {
-        seuils[iterateur-1] = *argv[iterateur];
+        seuils[iterateur-1] = atoi(argv[iterateur]);
     }
-    FILE *fichierEntree = stdin;
+    charPourInt = fgetc(fichierEntree);
+    nombreEval = atoi(&charPourInt);
+    
+    double ponderation [nombreEval];
+    double noteMax[nombreEval];
+    remplissage (ponderation, nombreEval, fichierEntree);
+    remplissage (noteMax,nombreEval,fichierEntree);
 
-    fscanf(fichierEntree, "%d", &nombreEval);
-    printf("%d",nombreEval);
-
-	/*printf("Entrer le code permanent de l'etudiant a evaluer sous format AAAA111111111111: ");
-	scanf("%s", codePerm);
-	
-	printf("Entrer les notes maximum permises, separees par un espace: ");
-	scanf("%s", notesPermises);
-	
-	
-	validerNotesFournies(argv[1]);
-	validerCodePerm(codePerm[0]);
-	validerValeursPermises(notesPermises[0], argv[1]);*/
-	
-	
-	return(0);
-
+    char codePerm[12];
+    fscanf(fichierEntree,"%s",codePerm);
+     validerCodePerm(codePerm);
+    
+    
+    return(0);
+    
 }
 
 
-
+void remplissage ( double tableaux[],int taille, FILE *fichier){
+    char charLu[10];
+    int i;
+    for (i= 0; i < taille; ++i) {
+        fscanf(fichier,"%s",charLu);
+        tableaux[i] = atof(charLu);
+    }
+    
+}
 
 
 /*void validerNotesFournies(const char *notes){  //need to handle floats
 	
 	if (sizeof(notes) > TAILLE) {
-		
-		fprintf(stderr, "%s: Trop de valeurs fournies \n", NOM);
-		exit(EXIT_FAILURE);
-		
+ 
+ fprintf(stderr, "%s: Trop de valeurs fournies \n", NOM);
+ exit(EXIT_FAILURE);
+ 
 	}
 	
 	for (int i = 0; i <= TAILLE; ++i){
 	
-		if {!isdigit(argv[i]){
-			fprintf(stderr, "%s: Au moins une note entree n'est pas conforme \n", NOM);
-			exit(EXIT_FAILURE);	
-		}
+ if {!isdigit(argv[i]){
+ fprintf(stderr, "%s: Au moins une note entree n'est pas conforme \n", NOM);
+ exit(EXIT_FAILURE);
+ }
 	
 	}
-
-}
-
-
-void validerCodePerm(char *codePermanent){
-	
-	for (i = 0; i < 12; i++){
-		
-		if (i < 4){
-			
-			!(isalpha(codePermanent[i]));
-			fprintf(stderr, "%s: Code Permanent non conforme \n", NOM);
-			exit(EXIT_FAILURE);
-		} else {
-			
-			!(isdigit(codePermanent[i]));
-			fprintf(stderr, "%s: Code Permanent non conforme \n", NOM);
-			exit(EXIT_FAILURE);			
-		}		
-	}	
-}
-
-
-void validerValeursPermises(char *notesPermises, const char *notes){
+ 
+ }
+ 
+ 
+ */
+void validerCodePerm(const char* codePerm){
+    int i;
+	for (i = 0; i < 4; ++i){
+        printf("%c",codePerm[i]);
+            if(!(isalpha(codePerm[i]))){
+            fprintf(stderr, ": Code Permanent non conforme \n");
+            exit(EXIT_FAILURE);
+            }
+    }
+    for (i=4; i<12; ++i) {
+        printf("%c",codePerm[i]);
+            if(!(isdigit(codePerm[i]))){
+            fprintf(stderr, "Code Permanent non conforme \n");
+            exit(EXIT_FAILURE);
+                
+            }
+    }
+ }
+ /*
+ 
+ void validerValeursPermises(char *notesPermises, const char *notes){
 	
 	int noteDonnee;
 	int notePermise;
-
+ 
 	for (int i = 0; i < TAILLE; i++){
-		noteDonnee = notes[i];
-		notePermise = notesPermises[i];
-		if (noteDonnee > notePermise){
-			fprintf(stderr, "%s: Au moins une note entree n'est pas conforme \n", NOM);
-			exit(EXIT_FAILURE);
-		}
+ noteDonnee = notes[i];
+ notePermise = notesPermises[i];
+ if (noteDonnee > notePermise){
+ fprintf(stderr, "%s: Au moins une note entree n'est pas conforme \n", NOM);
+ exit(EXIT_FAILURE);
+ }
 	}
 	
 	
-}*/
+ }*/
